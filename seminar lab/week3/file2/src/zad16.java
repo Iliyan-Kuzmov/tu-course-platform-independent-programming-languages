f50ec6818338b4b2e3a168f9868ec3fe564ef7b3
IliyanKuzmov <kuzmov.ilian@gmail.com>
2025-10-27 11:56:23 +0200

    27-10

--- seminar lab/week3/file2/src/zad16.java
+++ seminar lab/week3/file2/src/zad16.java
@@ -1,17 +1,30 @@
+import java.util.Arrays;
 public class zad16 {
     public static void main(String[] args) {
         int[] arr1 = {1,2,3};
         int[] arr2 = {2,3,4};
-        int countPovt = 0;
+        int countSech = 0;
+        int countObed = 0;
         for (int i = 0; i < arr1.length; i++) {
             for (int j = 0; j < arr2.length; j++) {
                 if (arr1[i] == arr2[j]) {
-                    countPovt++;
+                    countSech++;
                 }
             }
         }
-        int unionSize = arr1.length + arr2.length - countPovt;
-        int[] union = new int[unionSize];
+        countObed = arr1.length - countSech;
+        int[] sechenie = new int[countSech];
+        int[] obedinenie = new int[countObed];
+        for (int i =0 ; i< arr1.length;i++){
+            for (int j =0; j<arr2.length;j++){
+                for (int k = 0;k<countSech;k++){
+                    if (arr1[i] == arr2[j]){
+                        sechenie[k] = arr1[i];
+                    }
+                }
+            }
+        }
+        System.out.println(Arrays.toString(sechenie));
 
     }
 }
